648068c4150a53a3c91ba16da23822e348c6e42a
Mike Daleo <mike@novocent.com>
2018-04-04 14:28:03 -0400

    dev sync

---
+++ resources/js/app.js
@@ -0,0 +1,9 @@
+window.$ = require('jquery');
+
+import '../scss/app.scss';
+import './questions.js';
+
+
+window.test = 'TEsty pants';
+window.test2 = 'TEsty pants dos';
+window.test3 = 'TEsty pants dos';
---
+++ resources/js/questions.js
@@ -0,0 +1,21 @@
+window.questions = [
+	{
+		title: 'Question 1',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nisi corporis maxime sunt voluptatem at aspernatur asperiores debitis hic.',
+		type: 'single',
+		answers: [
+			{answer: 'Yes', value: 1},
+			{answer: 'No', value: 2},
+		]
+	},
+	{
+		title: 'Question 2',
+		desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, fugit nisi corporis maxime sunt voluptatem at aspernatur asperiores debitis hic.',
+		type: 'single',
+		answers: [
+			{answer: 'Yes', value: 1},
+			{answer: 'No', value: 2},
+			{answer: 'Potentially', value: 3},
+		]
+	},
+];
