ffe0a237507c5694f30166c29fa42e4426e86ca4
Mike Daleo <mike@mikedaleo.com>
2018-04-03 20:10:57 -0400

    init

---
+++ resources/app.js
@@ -0,0 +1,10 @@
+import './app.scss'
+
+window.Vue = require('vue');
+require('jquery');
+//require('sass-resources-loader');
+//require('bootstrap-loader');
+
+window.lol = 'works';
+
+
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+	mode: 'development',
+	entry: './resources/app.js',
+	output: {
+		filename: 'app.js',
+		path: path.resolve(__dirname, 'js'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader', 'sass-loader']
+				})
+			}
+		]
+	},
+	plugins: [
+		new ExtractTextPlugin('../css/app.css')
+	]
+};
